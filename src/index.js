d7884131a7fb2c32c721666fc39efbf51a298f27
Suchitra Naik <susanaik@gmail.com>
2018-10-08 19:52:45 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ToggleButton from './components/ToggleButton';
+import Address from './components/Address';
+
 
 ReactDOM.render(
   <div><ToggleButton /></div>,
